c44cc6e967a7730e366c3f61ad1f8ca56dd27452
Rahul Dubey <97272815+procoderahul@users.noreply.github.com>
2024-07-01 22:19:24 +0530

    Update User.java

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -2,48 +2,62 @@ package com.entity;
 
 public class User {
 	
-private int id;
-private String fullName;
-private String email;
-private String password;
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phoneNumber;
 
+    public User() {
+        super();
+    }
 
+    public User(String fullName, String email, String password, String phoneNumber) {
+        super();
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
 
-public User() {
-	super();
-	// TODO Auto-generated constructor stub
-}
+    // getters and setters
+    public int getId() {
+        return id;
+    }
 
-public User(String fullName, String email, String password) {
-	super();
-	this.fullName = fullName;
-	this.email = email;
-	this.password = password;
-}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getFullName() {
-	return fullName;
-}
-public void setFullName(String fullName) {
-	this.fullName = fullName;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getPassword() {
-	return password;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
+    public String getFullName() {
+        return fullName;
+    }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
