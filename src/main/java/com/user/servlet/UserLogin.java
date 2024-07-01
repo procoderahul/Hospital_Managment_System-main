40f7a5d773ccb3dd0d1b2109581401181e9ffade
Rahul Dubey <97272815+procoderahul@users.noreply.github.com>
2024-07-01 22:21:15 +0530

    Update UserLogin.java

--- src/main/java/com/user/servlet/UserLogin.java
+++ src/main/java/com/user/servlet/UserLogin.java
@@ -14,29 +14,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/userLogin")
-public class UserLogin extends HttpServlet{
+public class UserLogin extends HttpServlet {
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String email=req.getParameter("email");
-		String password=req.getParameter("password");
-		
-		
-		HttpSession session=req.getSession(); 
-		
-		UserDao dao=new UserDao(DBConnect.getConn());
-		User user=dao.login(email,password);
-		
-		if(user!=null) {
-			session.setAttribute("userObj",user);
-			resp.sendRedirect("index.jsp");
-		}
-		else {
-			session.setAttribute("errorMsg","invalid Email & Password");
-			resp.sendRedirect("user_login.jsp");
-		}
-		
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String identifier = req.getParameter("identifier");
+        String password = req.getParameter("password");
 
+        HttpSession session = req.getSession();
+
+        UserDao dao = new UserDao(DBConnect.getConn());
+        User user = dao.login(identifier, password);
+
+        if (user != null) {
+            session.setAttribute("userObj", user);
+            resp.sendRedirect("index.jsp");
+        } else {
+            session.setAttribute("errorMsg", "Invalid Email/Phone Number & Password");
+            resp.sendRedirect("user_login.jsp");
+        }
+    }
 }
